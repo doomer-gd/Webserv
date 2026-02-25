db20f020d2b732e751820af4e462c803545ceeee
IgorKulik-es <igorkulik28@proton.me>
2026-02-25 20:14:28 +0100

    added basic structure

--- include/parse/ParseConfig.hpp
+++ include/parse/ParseConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:16:19 by ikulik            #+#    #+#             */
-/*   Updated: 2026/02/24 21:41:41 by ikulik           ###   ########.fr       */
+/*   Updated: 2026/02/25 20:04:10 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,58 @@
 # define PARSE_CONFIG_HPP
 # include "../main/main.hpp"
 
+typedef std::string::iterator IterStr;
+
+enum EPasreState
+{
+	PS_REGULAR,
+	PS_DOUBLE_QUOTE,
+	PS_SINGLE_QUOTE,
+	PS_COMMENT,
+	PS_NUM_STATES
+};
+
+enum EConfigDictionary
+{
+	DC_MAIN_BODY,
+	DC_EVENTS,
+	DC_HTTP,
+	DC_SERVER,
+	DC_LOCATION,
+	DC_NUM_DICTS
+};
+
+class AParseState
+{
+	private:
+		const char*	expected;
+		inline bool	CheckExpected(char c);
+	public:
+		virtual StateStatus	InspectNext(std::istream& file, std::string& buffer);
+		virtual EPasreState	Exit();
+};
+
 class ConfigParser
 {
 	private:
 		Config			config;
 		std::ifstream	fileInput;
+		std::istream&	fileStream = fileInput;
+		std::map<std::string&, void (*)(std::string& value, void* object)>	dicts[DC_NUM_DICTS];
 		std::string		field;
 		std::string		value;
 
-		int	OpenFile(const std::string& fileName);
+		inline int	OpenFile(const std::string& fileName);
+		static bool	VerifyNumber(const std::string& str);
+		void		SetUpDictionaries();
+		int			ParseMainBody(const std::string& fileName);
+		int			ParseHTTP();
+		int			ParseServer(ServerConfig& config);
+		int			ParseLocation(LocationConfig& location);
 	public:
-		int				ParseConfigFile(const std::string& fileName);
-		const Config&	GetConfig() const;
+		int	GetConfig(Config& config, const std::string& fileName);
 };
+
 
 
 #endif
--- src/parse/ParseConfig.cpp
+++ src/parse/ParseConfig.cpp
@@ -6,13 +6,13 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 17:40:13 by ikulik            #+#    #+#             */
-/*   Updated: 2026/02/24 22:27:41 by ikulik           ###   ########.fr       */
+/*   Updated: 2026/02/25 20:11:15 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../main/main.hpp"
 
-int	ConfigParser::ParseConfigFile(const std::string& fileName)
+int	ConfigParser::GetConfig(Config& config, const std::string& fileName)
 {
 	OpenFile(fileName);
 	return 0;
@@ -24,4 +24,24 @@ int	ConfigParser::OpenFile(const std::string& fileName)
 	if (fileInput.fail())
 		throw Webserv::Except("failed to open configuration file: " + fileName);
 	return E_SUCCESS;
+}
+
+void	ConfigParser::SetUpDictionaries()
+{
+	// void	SetWorkerProcs(std::string& value, void* content)
+	// {
+	// 	if (VerifyNumber(value) == false)
+	// 		throw Webserv::Except("configuration file error");
+	// };
+}
+
+bool	ConfigParser::VerifyNumber(const std::string& str)
+{
+	std::string::const_iterator	iter = str.begin();
+	for (;iter != str.end();iter++)
+	{
+		if (isdigit(*iter) == 0)
+			return false;
+	}
+	return true;
 }
