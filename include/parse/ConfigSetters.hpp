95d734961d9714538e6c4270ed260dcb135178f1
IgorKulik-es <igorkulik28@proton.me>
2026-03-11 19:55:24 +0100

    finished config parser main loop, untested

--- include/parse/ConfigSetters.hpp
+++ include/parse/ConfigSetters.hpp
@@ -6,7 +6,7 @@
 /*   By: ikulik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 17:10:21 by ikulik            #+#    #+#             */
-/*   Updated: 2026/03/11 20:05:22 by ikulik           ###   ########.fr       */
+/*   Updated: 2026/03/11 20:06:11 by ikulik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,7 @@ class ConfigSetters
 		int	SetSingleParam(T& param, LineArray& args, EConfigDict scope, Verifier verify, T (*convert)(const std::string&));
 		template<typename T, typename L>
 		int	SetMultipleParam(T& param, LineArray& args, EConfigDict scope, Verifier verify, L (*convert)(const std::string&));
-	public:
-		ConfigSetters(ConfigMain& config);
 
-		int	SelectSetter(const std::string& nameParameter);
-		int	SetParameter(LineArray& args);
 		//main scope
 		int	SetErrorLog(LineArray& args);
 		int	SetEvents(LineArray& args);
