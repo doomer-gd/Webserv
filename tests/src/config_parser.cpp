313766bb8e7b5690a8a8e045dfd141ce873a3f00
IgorKulik-es <igorkulik28@proton.me>
2026-03-27 15:20:39 +0100

    added more tests, finished with the config parser

--- tests/src/config_parser.cpp
+++ tests/src/config_parser.cpp
@@ -17,18 +17,33 @@ std::ostream& operator<<(std::ostream& os, const std::pair<T, L>& v)
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+void printContainer(std::ostream& os, const T& container)
 {
+	typename T::const_iterator iter = container.begin();
 	os << "[";
-	for (size_t i = 0; i < v.size(); ++i) {
-		os << v[i];
-		if (i != v.size() - 1)
+	while (iter != container.end())
+	{
+		os << *iter;
+		iter++;
+		if (iter != container.end())
 			os << ", ";
 	}
 	os << "]\n";
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& container)
+{
+	printContainer(os, container);
 	return os;
 }
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::set<T>& container)
+{
+	printContainer(os, container);
+	return os;
+}
 
 void	printConfigContent(ConfigMain& config)
 {
@@ -42,15 +57,22 @@ void	printConfigContent(ConfigMain& config)
 	std::cout << "keepalive_timeout: " << config.timeOut.keepAlive << std::endl;
 	if (config.servers.size() > 0)
 	{
-		if (config.servers[0].portsArray.size() > 0)
-			std::cout << "listen: " << config.servers[0].portsArray << std::endl;
-		if (config.servers[0].serverNames.size() > 0)
-			std::cout << "server_name: " << config.servers[0].serverNames << std::endl;
+		std::cout << "listen: " << config.servers[0].portsArray << std::endl;
+		std::cout << "server_name: " << config.servers[0].serverNames << std::endl;
+		std::cout << "client_body_size: " << config.servers[0].clientMaxBodySize << std::endl;
 		if (config.servers[0].errorPages.size() > 0)
 			std::cout << "error_page: " << *(config.servers[0].errorPages.begin()) << std::endl;
+		if (config.servers[0].locations.size() > 0)
+		{
+			std::cout << "loc root: " << config.servers[0].locations.begin()->root << std::endl;
+			std::cout << "loc index: " << config.servers[0].locations.begin()->index << std::endl;
+			std::cout << "loc autoindex: " << config.servers[0].locations.begin()->autoindex << std::endl;
+			std::cout << "loc methods: " << config.servers[0].locations.begin()->methods << std::endl;
+			std::cout << "loc redirect: " << config.servers[0].locations.begin()->redirect << std::endl;
+			std::cout << "loc cgi_redirect: " << config.servers[0].locations.begin()->cgiExtension  << " " << config.servers[0].locations.begin()->cgiPath << std::endl;
+		}
 	}
 }
-
 
 int main(int argc, char** argv)
 {
@@ -62,6 +84,9 @@ int main(int argc, char** argv)
 	if (parser.ParseConfigFile(config, argv[1]) == E_SUCCESS)
 		printConfigContent(config);
 	else
+	{
 		std::cerr << "Config file parsing failed" << std::endl;
+		std::cerr << "Error at: " << parser.GetErrorLine() << std::endl;
+	}
 	return (0);
 }
